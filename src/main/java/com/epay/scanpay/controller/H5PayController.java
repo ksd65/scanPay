4895c3a9324c19f3969eeb8a516627af5ffe7212
linxf <linxf@doone.com.cn>
2017-12-21 18:11:29 +0800

    h5

--- src/main/java/com/epay/scanpay/controller/H5PayController.java
+++ src/main/java/com/epay/scanpay/controller/H5PayController.java
@@ -2,12 +2,17 @@ package com.epay.scanpay.controller;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import net.sf.json.JSONObject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,8 +21,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.epay.scanpay.common.constant.SysConfig;
 import com.epay.scanpay.common.utils.CommonUtil;
+import com.epay.scanpay.common.utils.EpaySignUtil;
 import com.epay.scanpay.common.utils.HttpUtil;
 import com.epay.scanpay.common.utils.IpUtils;
+
 
 
 
@@ -37,14 +44,53 @@ public class H5PayController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		String privateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCIxkLM+QBKTqw9BsCSjiLw5pO186HwgMOZIXUFDVnRsYyPeiFsnPgaNQmT7L5Ur0jHtKc0yUdk5jd8BG06N40474MiBzNNNJJa4mkPQfUlsD0T0NMLesjFthKqyQxS1cMqpz8b372pwsWb16BVM/9i5ak9JgqguAtEkxR2lSNd0sn/GEqH9hdw6RvMH/Y/ePztdA3bANJL64g0hrKKBW0YcJvXJpKF8vIwuNuamOoKOzlEIPZC05331j4c8xw/EUiBbS9sELAT+MLNlYXHIWtpDDbJTNZGKXDskJpfae7ifW4TU6ZlraiW4JGabl1d5DH2t4k5NnFqiquPZWPhZ8u9AgMBAAECggEAdGdHsuq4NIWAUO/ONOyDEEMss04GJIlx7oFq9kHGj5Br8DAhAi8VeDhrTlnOIoSLjGtTYrlq6ZSE5CdgTou4xRwSnoNCRhLX/EF06GdaHBlB4ft3oe19scajXHZ+5oDG+SYdr7tbz37Uby20Zs86KxEKV+BoayA3dsU2RTXoQ5BCtMeXaIUif3JjHGy1xHkR1MPN2vXvwMqU7Femk/Qr9s8W+9IgVp5ah28Kx5qkR63TkEONyYTbI+U4El/bVnZmcaHL+/KJ9wo8+/7hHrl+mWV0T3iKa4tJWed9V+IUKrLeUpn9DOIv9WNlDFjJqBckC/GBhujG7uRKWfminGRAgQKBgQDJnQv9HAgUnGsYTayY1iW2CwgFZJn1RgaTCjo1Oc6tDTFXdP4uIFfxEfnLhGuRuItTKHW1hf1Hp4n+wiZO5q1HjjC3bLBUy1Lw+HvaJLNHRkCye58cg2Jlu81girzciazLiHqfEaZxFsUq8WsqzZIBRr0gPDDJMrSOBfkf+hWckwKBgQCtq5WSq/t5Cr1N5AOoYoBDvJOHqQ4/6Ue0JF270/u1H/qdMQ9w/OQRixhHNP7g5oiPBWKPQTs3uU9/SHpX5CHsyqlQDvOkrSnESp4pz0BUOT971er/8o79NrUE9ZQMRDhu0WHXtbPOq+7siUw9SA5HcEh8Hd/Y3xLOFhvw90x4bwKBgGBGFRZ9j0JAW0eUt8mX4RQn+mGQ44/jK3qFlLwb6ZxrQ1eO712ZZkUgn1bW2gMQy78e/+55mDPiRhwYG/DraG1V8d91EFK9cNLO5V2Kzu1HF9fi/lzARHluD6l9NqhdOd1LQ7q30/IGvIpAFDuxRHpFjERbWbSJ+Pwk0Ay8ABvvAoGAah4XFfkqfqqWQ3rY1VHiyAD5MIKXJ2w2mRdDgxqjiegRbW1l3wdXoHSakCAMwYV72dBTie807Pa5Ya/6uau3IwYucLHCJFR+2ecyP5/Y0d3tMZDjuCMRRh3gfDhGjzw8M1KTc4geZ2Fda4D1adiWiQZN9DEY715XEkAmMJYbTtcCgYEAn9BcQcpAsbV2fpmO0VU8CMFoTnbfYpmHR9N6A45r72j1nQmJxfZbvL7JiIK7jhRUuQERC+jK08FPKbFDoE5a7UxNiKDE/ZtuEwPehrf8yVenxmAWMh4llc1HoWQfjCY8BJuAm3K4jHtswB0+PQfTMG7PVisoMfdVEJ4gq8BLAv0=";
+		//待签名字符串
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("orderNum", orderNum); 
+		params.put("memberCode", "9010000988");  
+		params.put("callbackUrl",  "www.baidu.com");
+		params.put("payMoney", "0.01" );
+		params.put("sceneInfo", "测试" );
+		params.put("ip",  ip);
+	
+		
+ 		String srcStr = orderedKey(params);
+ 		System.out.println("srcStr==="+srcStr);
+		String signstr = EpaySignUtil.sign(privateKey, srcStr);
+		
+		
 		model.addAttribute("orderNum", orderNum);
 		model.addAttribute("ip", ip);
-		model.addAttribute("signStr", "11111");
+		model.addAttribute("signStr", signstr);
 		
 		return "payment/wxH5Pay";
 		
 	}
 	
+	
+	private String orderedKey(Map<String, String> dataMap){
+		ArrayList<String> list = new ArrayList<String>();
+		for(String key:dataMap.keySet()){
+			if(dataMap.get(key)!=null&&!"".equals(dataMap.get(key))){
+				list.add(key + "=" + dataMap.get(key));
+			}
+		}
+		int size = list.size();
+		String [] arrayToSort = list.toArray(new String[size]);
+		Arrays.sort(arrayToSort, String.CASE_INSENSITIVE_ORDER);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size; i ++) {
+			sb.append(arrayToSort[i]);
+			sb.append("&");
+		}
+		String result = sb.toString();
+		if(StringUtils.isNotBlank(result)){//去掉最后的&
+			result = result.substring(0, result.length()-1);
+		}
+		return result;
+	}
 	
 	@RequestMapping("/payment/toWxH5")
 	public String toPayment(Model model,HttpServletRequest request){
